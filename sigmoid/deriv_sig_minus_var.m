5f70363d10807d28b0de2a17aaacebd1fa3fd49d
Catherine Yeh <catherine16388@gmail.com>
2021-10-04 11:45:36 -0400

    fix deriv_sig

--- sigmoid/deriv_sig_minus_var.m
+++ sigmoid/deriv_sig_minus_var.m
@@ -2,7 +2,7 @@ function dy = deriv_sig_minus_var(y, c, epsilon)
 % Inputs
 %       y: variable
 %       c: constant
-dy = (1 / epsilon) * exp((c - y) / epsilon) * (1 + exp((c - y) / epsilon))^(-2);
+dy = (-1 / epsilon) * exp((c - y) / epsilon) * (1 + exp((c - y) / epsilon))^(-2);
 
 end
 
