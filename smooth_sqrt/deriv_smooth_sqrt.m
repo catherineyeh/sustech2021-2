77a0fa6c368b498aa5ac49d26b3fecac46fee15b
chapmanmp <chapmanmp@users.noreply.github.com>
2021-10-04 15:03:59 -0400

    writing square root functions in terms of separate cases

--- smooth_sqrt/deriv_smooth_sqrt.m
+++ smooth_sqrt/deriv_smooth_sqrt.m
@@ -1,11 +1,27 @@
+%from Duan & Lian 2016 "Smoothing Approximation to the Square-Root Exact
+%Penalty Function," p. 89
+
 function dphi_dy = deriv_smooth_sqrt(y)
+
     epsilon = 0.01;
-    case1 = y <= 0;
-    case2 = 0 < y && y <= epsilon;
-    case3 = y > epsilon;
+    case1 = (y <= 0);
+    case2 = (0 < y && y <= epsilon);
+    case3 = (y > epsilon);
     
-    dphi_dy = 0 * case1 + ...
-        (1/(2*epsilon)) * sqrt(y) * case2 + ...
-        (1/2) * (1/sqrt(y)) * case3;
+    if case1
+        dphi_dy = 0;
+    elseif case2
+        dphi_dy = (1/(2*epsilon)) * sqrt(y);
+    elseif case3
+        dphi_dy = (1/2) * (1/sqrt(y));
+    else
+        error('issue with deriv_smooth_sqrt cases');
+    end
+        
+        
+    % if division by zero could cause an issue, so I think it's better to write out cases individually as above.    
+    %dphi_dy = 0 * case1 + ...
+     %   (1/(2*epsilon)) * sqrt(y) * case2 + ...
+      %  (1/2) * (1/sqrt(y)) * case3;
 end
 
--- smooth_sqrt/smooth_sqrt.m
+++ smooth_sqrt/smooth_sqrt.m
@@ -1,11 +1,29 @@
 function phi_y = smooth_sqrt(y)
-% from Duan & Lian 2016
+% from Duan & Lian 2016 "Smoothing Approximation to the Square-Root Exact
+% Penalty Function," p. 89
     epsilon = 0.01;
-    case1 = y <= 0;
-    case2 = 0 < y && y <= epsilon;
-    case3 = y > epsilon;
-    phi_y = (2/3) * sqrt(epsilon) * case1 + ...
-        (1/(3*epsilon)) * y^(3/2) + (2/3) * sqrt(epsilon) * case2 + ...
-        sqrt(y) * case3;
+    case1 = (y <= 0);
+    case2 = (0 < y && y <= epsilon);
+    case3 = (y > epsilon);
+    
+    if case1
+        phi_y = (2/3) * sqrt(epsilon);
+    elseif case2
+        phi_y = (1/(3*epsilon)) * y^(3/2) + (2/3) * sqrt(epsilon);
+    elseif case3
+        phi_y = sqrt(y);
+    else
+        error('issue with smooth_sqrt cases');
+    end
+    
+    % an issue could arise if a negative y is raised to 3/2, so I've
+    % re-written in terms of separate cases above.
+    %
+    % phi_y = (2/3) * sqrt(epsilon) * case1 + ...
+    %    ((1/(3*epsilon)) * y^(3/2) + (2/3) * sqrt(epsilon)) * case2 + ...
+    %    sqrt(y) * case3;
+    % in line 22, parentheses are needed for (terms_for_case_2) * case2.
 end
+
+
 
