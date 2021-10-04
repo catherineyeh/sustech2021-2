bdf6d7ec1869bd0756a8b2aa9e2cb0c7de8e7c4b
chapmanmp <chapmanmp@users.noreply.github.com>
2021-10-04 16:53:39 -0400

    some corrections to deriv_qoute

--- dynamics/deriv_qoute.m
+++ dynamics/deriv_qoute.m
@@ -1,6 +1,18 @@
 function [dqdx1, dqdx2, dqdu] = deriv_qoute(x1, config)
-    dqdx1 = config.cout * deriv_smooth_sqrt(x1);
-    dqdx2 = 0;
-    dqdu = 0;
+
+gamma = (x1 / config.a1) - config.zo;
+
+dqdx1 = config.cout * deriv_smooth_sqrt(gamma) * 1/config.a1;
+
+dqdx2 = 0;
+
+dqdu = 0;
+
 end
+
+% Remember the chain rule. Before it was written:
+% dqdx1 = config.cout * deriv_smooth_sqrt(x1);
+% but qout_answer = config.cout * smooth_sqrt(gamma);
+% so we need to take the derivative of gamma as well.
+  
 
