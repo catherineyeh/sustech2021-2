663d26c6c68e10795af5e59be6b06b928e82367f
chapmanmp <chapmanmp@users.noreply.github.com>
2021-10-04 15:20:37 -0400

    potential correction to sigmoid derivative

--- sigmoid/deriv_sig_minus_var.m
+++ sigmoid/deriv_sig_minus_var.m
@@ -1,8 +1,14 @@
-function dy = deriv_sig_minus_var(y, c, epsilon)
 % Inputs
 %       y: variable
 %       c: constant
-dy = (-1 / epsilon) * exp((c - y) / epsilon) * (1 + exp((c - y) / epsilon))^(-2);
+
+function dy = deriv_sig_minus_var(y, c, epsilon)
+
+% dy = (-1 / epsilon) * exp((c - y) / epsilon) * (1 + exp((c - y) / epsilon))^(-2);
+
+%Please check below. I believe that it should be +1/epsilon not -1/epsilon.
+
+dy = (1 / epsilon) * exp((c - y) / epsilon) * (1 + exp((c - y) / epsilon))^(-2);
 
 end
 
--- sigmoid/sig_minus_var.m
+++ sigmoid/sig_minus_var.m
@@ -1,9 +1,13 @@
-function out = sig_minus_var(y, c)
+%We call this "sig_minus_var" since -y is included in the sigmoid function
 % Inputs
 %       y: variable
 %       c: constant
 %       epsilon: parameter in sigmoid function
+
+function out = sig_minus_var(y, c)
+
     epsilon = 0.01;
+    
     out = 1 / (1 + exp((c - y) / epsilon));
 
 end
