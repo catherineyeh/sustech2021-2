89d2c1a9db96193e7973f49ad7285d3f644f8551
Catherine Yeh <catherine16388@gmail.com>
2021-10-04 21:03:11 -0400

    fix A_tilde or A_hat

--- matrices/A_tilde_matrix.m
+++ matrices/A_tilde_matrix.m
@@ -6,8 +6,10 @@ function A_tilde = A_tilde_matrix(N, Ap_bar)
 dimi = size(Ap_bar,1);
 dimj = size(Ap_bar,2);
 A_tilde = zeros(N*dimi, dimj);
-for i=1:2:N*dimi
-    A_tilde(i:i+dimi-1,:) = Ap_bar^i;  % The syntax A^2 is equivalent to A*A
+power = 1;
+for i=1:dimi:N*dimi
+    A_tilde(i:i+dimi-1,:) = Ap_bar^(power);  % The syntax A^2 is equivalent to A*A
+    power = power + 1;
 end
 
 end
